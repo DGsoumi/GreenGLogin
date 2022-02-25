7206e71f70a073fe68ba57e799a63368fbc352ca
DGsoumi <soumidasguptauemkcse2016@gmail.com>
2022-02-25 19:28:15 +0530

    Initial Commit

---
+++ src/test/java/io/aadeesh/UserRegistration1ApplicationTests.java
@@ -0,0 +1,15 @@
+package io.aadeesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@SpringBootConfiguration
+class UserRegistration1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
